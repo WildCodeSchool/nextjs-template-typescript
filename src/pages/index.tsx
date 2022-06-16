61463a30d55806e0ba480cfee7880859643dc82f
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2022-06-16 09:37:53 +0200

    Added src folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Index.module.css";
-import Logo from "../public/logo.png";
+import Logo from "../../public/logo.png";
 import CounterComponent from "../components/CounterComponent";
 
 const Home: NextPage = () => {
